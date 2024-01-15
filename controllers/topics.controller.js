fda6f48727f4828666986304d037118af8fd41f4
keaysb <keays.b@yahoo.com>
2024-01-15 11:47:24 +0000

    complete question 2 (get topics) with tests. Also, amend enviroment variables to not include ";"

---
+++ controllers/topics.controller.js
@@ -0,0 +1,7 @@
+const {fetchTopics} = require('../models/topics.models')
+
+exports.getTopics = (req, res, next) => {
+    fetchTopics().then(({rows})=> {
+        res.status(200).send({topics : rows})
+    })
+}
