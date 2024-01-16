e86231c28f6a8ca26066be6011c0d0c9c8f87e8b
keaysb <keays.b@yahoo.com>
2024-01-16 14:55:36 +0000

    complete all feedback changes

--- error-handlers/handlePsqlErrors.js
+++ error-handlers/handlePsqlErrors.js
@@ -1,4 +1,4 @@
-exports.handlePsqlErrors = (err, req, res, next) => {
+module.exports = (err, req, res, next) => {
     if (err.code === '22P02') {
       res.status(400).send({ msg: 'Bad Request' });
     } else next(err);
